29a1d3220594b67ef985b4f49b4503575379e01b
myunghwanKang <myunghwan0421@gmail.com>
2021-10-29 15:46:31 +0900

    post

--- _posts/CS/datastructure/test.py
+++ _posts/CS/datastructure/test.py
@@ -1,26 +1,6 @@
-class Node:
-    def __init__(self, key = None):
-        self.key = key
-        self.next = None
-
-    def __str__(self):
-        return str(self.key)
-
-    
-
-
-class SinglyLinkedList:
-    def __init__(self):
-        self.head = None
-        self.size = 0
-
-    def __len__(self):
-        return self.size
-
-
-    def pushFront(self, key): # 2 -> 1 2
-        new_node = Node(key) # new_node = Node(1)
-        new_node.next = self.head # new_node.next = Node(2)
-        self.head = new_node # self.head = Node(1)
-        self.size += 1
-
+keys = input().split()
+values = map(int, input().split())
+ 
+x = dict(zip(keys, values))
+x = {key:value for key, value in x.items() if key != 'delta' and value != 30}
+print(x)
