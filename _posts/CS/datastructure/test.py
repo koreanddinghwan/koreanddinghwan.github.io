8aff81f3f4a7def47b8241be59b7993ad4a3cbc8
myunghwanKang <myunghwan0421@gmail.com>
2021-11-01 01:49:35 +0900

    post

--- _posts/CS/datastructure/test.py
+++ _posts/CS/datastructure/test.py
@@ -1,299 +1,36 @@
+class Time:
+    def __init__(self, hour, minute, second):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
 
-
-
-# class Node:
-#     def __init__(self, key):
-#         self.key = key
-#         self.next = None
-
-#     def __str__(self):
-#         return str(self.key)
-
-
-
-# class singlylinkedlist:
-#     def __init__(self):
-#         self.head = None
-#         self.size = 0
-
-#     def __iter__(self):
-#         v = self.head
-#         while v != None:
-#             yield v
-#             v = v.next
-        
-#     def __str__(self):
-#         return "->".join(str(v) for v in self)
-        
-#     def __len__(self):
-#         return self.size
-
-#     def pushfront(self, key):
-#         new_node = Node(key)
-#         new_node.next = self.head
-#         self.head = new_node
-#         self.size += 1
-
-#     def pushback(self,key):
-#         new_node = Node(key)
-#         if self.size == 0:
-#             self.head = new_node
-
-#         else:
-#             tail = self.head
-#             while tail.next != None:
-#                 tail = tail.next
-
-#             tail.next = new_node
-#         self.size += 1
-
-#     def popfront(self):
-#         if self.size == 0:
-#             return None
-#         else:
-#             x = self.head #|node(4)를 복사
-#             key = x.key#|key 는 node(4)의 key인 4
-#             self.head = x.next #|node(4).next = node(5)이므로 self.head 는 node(5)가 된다.
-#             self.size -= 1
-#             del x
-#             return key
-
-#     def popback(self):
-#         if self.size == 0: #노드가 0개인 경우
-#             return None
-
-#         else:
-#             prev, tail = None, self.head #prev에 tail을 지정해나가면서 tail.next가 none인걸 찾는다.
-#             while tail.next != None: #노드가 1개인 경우
-#                 prev = tail
-#                 tail = tail.next
-
-#             if prev == None:  #while 연산이 끝났는데, prev가 none인건 노드가 1개일때밖에 없다.
-#                 self.head == None #pop하면 리스트가 비워지므로 self.head를 None으로 만들어 
-#             else:
-#                 prev.next = None #노드가 2개이상인 경우, prev 노드가 마지막노드가 되므로 prev의 next(링크)를 지운다.
-
-#             key = tail.key
-#             del tail
-#             self.size -= 1
-#             return key
-
-#     def search(self,key):
-#         for v in self:
-#             if v.key == key:
-#                 return v
-
-#         return None
-
-#     def remove(self, key):
-#         if self.size == 0: #연결리스트 길이 0인 경우
-#             return None
-
-#         elif self.head.key == key: #리스트의 헤드노드와 찾고자하는 노드의 키가 같은경우
-#             self.popfront() #popfront로 다음 노드를 헤드노드로 변경, 본래 헤드노드삭제
-
-#         else:
-#             prev, tail = None, self.head #초기값설정
-#             while tail.next != None: #tail.next가 none이 아닌동안
-#                 prev = tail #prev와 tail을 한칸씩 옮겨가기
-#                 tail = tail.next
-#                 if tail.key == key: #만약 tail.key와 찾고자하는 키가 같다면
-#                     prev.next = tail.next #remove할 node의 링크를 지워버리면된다.
-#                     del tail #tail node를 리스트 상에서 삭제
-#                     self.size -= 1 #한개 삭제되었으므로 리스트 길이 1개 줄인다.
-#                     break
-            
-#     def reverse(self):
-#         a, b = None, self.head
-#         while b:
-#             if b:
-#                 c = b.next #c에 b의 다음노드를 복사
-#                 b.next = a #b의 링크를 반대로 설정
-#             a = b
-#             b = c
-
-            
-
-
-
-
-# test = singlylinkedlist()
-# test.pushfront(5)
-# test.pushfront(4)
-# test.pushfront(3)
-# print(test)
-
-# a, b = None, test.head
-# c = b.next
-# b.next = a
-# print(c)
-# print(a)
-
-
-
-
-
-
-
-class Node:
-    def __init__(self, key = None):
-        self.key = key
-        self.next = self
-        self.prev = self
-
-    def __str__(self):
-        return str(self.key)
-
-
-class Doublylinkedlist:
-    def __init__(self):
-        self.head = Node()
-        self.size = 0
-
-    def __iter__(self):
-        v = self.head.next
-        while v != self.head: #양방향리스트는 연결되어있으므로 None이 출력되는순간 멈춰야함
-            yield v
-            v = v.next
-
-        
-    def __str__(self):
-        return "->".join(str(v) for v in self if v != None)
     
-    def __len__(self):
-        return self.size
+    # @classmethod
+    # def from_string(cls, string):
+    #     hour,minute,second = map(int, string.split(":"))
+    #     return cls(hour,minute,second)
 
+    @staticmethod
+    def from_string(string):
+        hour,minute,second = map(int, string.split(":"))
+        return Time(hour,minute,second)
 
-    def splice(self, a, b, x):
-        #경우(1) cut #| ap a .. .. b bn -> ap bn
-        ap = a.prev
-        bn = b.next
-
-        ap.next = bn
-        bn.prev = ap
-
-        #경우(2) paste #| x xn -> x a .. .. .. b xn
-        xn = x.next #원래 x의 다음 노드를 xn이라고 할당
-        x.next = a
-        a.prev = x
-
-        xn.prev = b
-        b.next = xn
-
-    #이동연산
-    def pushfront(self, key):
-        self.insertAfter(self.head, key) #self.head는 더미노드. self.head의 다음값은 리스트의 처음 값
-   
-    def insertAfter(self, x, key): #새로운 노드를 생성해 x뒤에
-        self.moveAfter(Node(key), x) #키로 노드를 생성하면 초기에 prev,next가 자기자신이다.
-        self.size += 1
-   
-    def moveAfter(self,a,x): #| ap a an | xp x xn 
-        # -> |ap an| xp x a xn
-        self.splice(a,a,x) #와 동일.
         
-    def pushback(self, key):
-        self.insertBefore(self.head, key) #self.head는 더미노드. self.head의 이전값은 리스트의 마지막값
-
-    def insertBefore(self, x, key):
-        self.moveBefore(Node(key), x)
-        self.size += 1
-
-    def moveBefore(self,a,x): #a를 x뒤로 붙이기
-        self.splice(a,a,x.prev) #와 동일
-
-    #search
-    def search(self,key):
-        for i in self:
-            if i.key == key:
-                return i
-        else:
-            return None
-
-    #isempty
-    def isEmpty(self):
-        if self.size != 0:
-            return False
-        else:
-            return True
-
-    #last,first
-    def first(self):
-        ch = self.head
-        return ch.next
-
-    def last(self):
-        ch = self.head
-        return ch.prev
+    @staticmethod
+    def is_time_valid(string):
+        hour,minute,second = map(int, string.split(":"))
+        return hour <= 24 and hour >= 0 and minute <= 59 and minute >= 0 and second >= 0 and second <= 60
 
 
-    #삭제연산
-    def remove(self,x): #x노드를 삭제.
-        if x == None or x == self.head:
-            pass
-        else:
-            x.prev.next = x.next #노드 x의 이전 노드의 링크는 x.next
-            x.next.prev = x.prev #노드 x의 다음 노드의 prev링크는 x.prev
-            del x
-            
-    def popfront(self):
-        if self.isEmpty():
-            return None
-        else:
-            key = self.head.next.key
-            self.remove(self.head.next)
-            return key
 
-    def popback(self):
-        if self.isEmpty():
-            return None
 
-        else:
-            key = self.head.prev.key
-            self.remove(self.head.prev)
-            return key
 
-    def join(self, list):
-        if self.isEmpty():
-            self = list
-        elif list.isEmpty():
-            self = self
-        else:
-            self.head.prev.next = list.head.next #self 리스트의 마지막값의 링크는 추가하고자하는 list의 head노드 다음 값이다.
-            list.head.next.prev = self.head.prev #추가하고자하는 리스트의 첫값의 prev링크는 self리스트의 마지막값
-            list.head.prev.next = self.head #추가하고자하는 리스트의 마지막값의 다음값은 self리스트의 헤드값이되어 서로 원형 연결한다.
-            self.head.prev = list.head.prev #self.head의 prev링크는 list의 마지막값이되어야한다.
-    
+time_string = input()
 
-a = Doublylinkedlist()
-a.pushfront(5)
-# insertafter(self.head, 5)
-# moveafter(Node(5),self.head)
-#splice(Node(5), Node(5), self.head)
-#def splice(self, Node(5), Node(5), self.head):
-        #경우(1) cut #| ap a .. .. b bn -> ap bn
-        # ap = Node(5).prev -> 자신
-        # bn = Node(5).next -> 자신
-
-        # ap.next = bn
-        # bn.prev = ap
-
-        # #경우(2) paste #| x xn -> x a .. .. .. b xn
-        # xn = x.next #원래 x의 다음 노드를 xn이라고 할당 
-        # x.next = a
-        # a.prev = x
-
-        # xn.prev = b
-        # b.next = xn
-
-a.pushback(4)
-print(a)
-
-print(a)
-
-b = Doublylinkedlist()
-b.pushback(10)
-b.pushback(15)
-a.join(b)
-print(a)
-print(a.head.prev.next)
+if Time.is_time_valid(time_string): #정적메서드, 입력받아 truefalse리턴
+    t = Time.from_string(time_string) #t라는 인스턴스를 만들어줘야한다. 
+    #인스턴스를 어떻게 만들었었지?-->t = Time(hour,minute,second)
+    #from_string은 리턴값으로 
+    print(t.hour, t.minute, t.second)
+else:
+    print('잘못된 시간 형식입니다.')
--- _posts/python/syntax/2021-10-29-classmethod.md
+++ _posts/python/syntax/2021-10-29-classmethod.md
@@ -11,7 +11,7 @@ toc: true
 toc_sticky: true
 
 date: 2021-10-29
-last_modified_at: 2021-10-29
+last_modified_at: 2021-10-31
 ---
 
 # 클래스와 메서드의 관계
@@ -93,14 +93,15 @@ print(isinstance(a,list))
 <br>
 <br>
 
+# 속성 
 
-# 클래스와 속성
+## 인스턴스 속성
 
-클래스의 속성은  `__init__()`메서드 안에서 self.속성에 값을 할당한다.
+클래스의 인스턴스 속성은  `__init__()`메서드 안에서 self.속성에 값을 할당한다.
 
 ```python
 class stack:
-    def __init__(self): #클래스의 개게생성함수.
+    def __init__(self): #클래스의 객체생성함수.
             self.items = []
 ```
 속성을 확인하기 위해서 객체를 만들어 속성을 호출해보면,
@@ -122,7 +123,7 @@ test라는 인스턴스는 stack 클래스가 설정되었고,
 <br>
 <br>
 
-## self란
+### self란
 
 self는 인스턴스 자기자신을 의미한다.
 ```python
@@ -168,6 +169,8 @@ print(st.__value)
 속성에 __를 붙이게되면 비공개 속성으로 설정되어 클래스 바깥에서 접근할 수 없다.  
 비공개클래스는 클래스 내부의 메서드에서만 사용가능하다.  
 
+<br><br>
+
 # 클래스 속성
 
 클래스 속성은 인스턴스 속성과 달리  
@@ -198,13 +201,136 @@ print(test2.items)
 ```
 이렇게 클래스 속성의 경우, 각각 다른 인스턴스가 공유할 수 있다.  
 
+<br><br>
+
+## 비공개 클래스 속성
+
+앞서 우리는 비공개 속성을 설정하는 방법에 대해 공부했었다.  
+비공개 인스턴스 속성은 self.__ 로 선언했었는데,  
+비공개 클래스 속성은 self. 없이 __만 붙여서 선언하면 클래스 전체에서 공유할 수 있다.
+
+<br><br>
 
 ## 속성, 메서드 이름을 찾는 순서
 
+지금까지 인스턴스 속성과 클래스 속성 2가지에 대해서 공부했다.  
+그런데, 속성이 선언되었을때, 파이썬은 이 속성을 찾기위해 어떤 절차를 밟을까?  
+파이썬은 속성, 메서드 이름을 찾을 때 인스턴스, 클래스 순으로 찾는다.  
+그래서 인스턴스 속성에 없으면 클래스 속성을 찾는다.  
+이를 알기 위해서는 인스턴스와 클래스에서 각각 `__dict__` 속성을 호출하면  
+현재 인스턴스와 클래스의 속성을 딕셔너리 형태로 알 수 있다!
+
+<br><br>
+
+# 메서드
+
+## 정적 메서드
+
+클래스의 메서드를 선언할때 인스턴스를 통해 호출했다.
+예시
+```python
+def push(self,val): #삽입
+        self.items.append(val)
+```
+
+하지만 정적 메서드는 인스턴스를 통하지 않고 클래스에서 바로 호출할 수 있다.  
+정적 메서드는 메서드 위에 @staticmethod를 붙이고, 매개변수에 self를 지정하지 않는다.
+
+```python
+class classname:
+    @staticmethod
+    def add(a,b):
+        return a+b
+```
+
+1. 정적 메서드는 self를 받지 않기때문에 인스턴스 속성에 접근할 수 없다.
+2. 정적 메서드는 메서드의 실행이 외부 상태에 영향을 주지 않는 순수함수를 만들때 사용한다.
+3. 순수 효과란 부수효과가 없고, 입력 값이 같으면 항상 같은 출력 값을 반환한다는 것을 의미한다.
+4. 인스턴스 상태에 변화를 주지 않는다.
+
+따라서
+<b>인스턴스의 내용을 변경할때는 인스턴스 메서드를 사용하고,  
+결과만 필요한 경우에는 정적 메서드를 사용한다.</b>
+
+<br><br>
+
+## 클래스 메서드
+
+클래스 메서드의 경우, 메서드 위에 @classmethod를 붙인다.  
+그리고 첫 번째 매개변수로 cls를 지정해야한다.
+```python
+class classname:
+    @classmethod
+    def method(cls, a,b)
+        return a
+```
+
+클래스 메서드는 메서드 내에서 클래스 메서드와 클래스 속성에 접근해야할때 사용된다.  
+또한 클래스 메서드는 정적메서드와 같이 인스턴스가 필요없다는 것이 특징이다.
+
+<br><br>
+
+# 심사문제
+
+```python
+class Time:
+    def __init__(self, hour, minute, second):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+    
+    # @classmethod 방법 1
+    # def from_string(cls, string):
+    #     hour,minute,second = map(int, string.split(":"))
+    #     return cls(hour,minute,second)
+
+    @staticmethod #방법 2
+    def from_string(string):
+        hour,minute,second = map(int, string.split(":"))
+        return Time(hour,minute,second)
+
+        
+    @staticmethod
+    def is_time_valid(string):
+        hour,minute,second = map(int, string.split(":"))
+        return hour <= 24 and hour >= 0 and minute <= 59 and minute >= 0 and second >= 0 and second <= 60
 
 
 
+time_string = input()
 
-# 인스턴스 속성
+if Time.is_time_valid(time_string): #정적메서드, 입력받아 truefalse리턴
+    t = Time.from_string(time_string) #t라는 인스턴스를 만들어줘야한다. 
+    #인스턴스를 어떻게 만들었었지?-->t = Time(hour,minute,second)
+   
+    print(t.hour, t.minute, t.second)
+else:
+    print('잘못된 시간 형식입니다.')
+```
 
+<br><br>
+
+## 해설
+
+is_time_valid는 매개변수로 시간을 입력받아 true, false만 리턴하면되므로   
+정적 메서드를 사용한다.  
+
+t가 t.hour, t.minute 등으로 인스턴스 속성에 접근가능해야하므로,  
+t는 인스턴스가 되어야한다.  
+따라서 Time.from_string()는 인스턴스를 리턴해야한다.  
+
+<br><br>
+
+인스턴스를 리턴하는 방법에는 2가지가 있는데,    
+1번째 방법으로 클래스 메서드를 사용하는 방법이다.    
+우리가 클래스 메서드에서 선언하는 cls는 그 자체로 클래스를 의미한다.    
+@classmethod 선언 이후, 첫 번째 매개변수로 cls, 리턴값으로  
+cls(hour,minute,second)를 적는다.  
+
+<br><br>
+
+2번째 방법으로 정적 메서드를 사용한다.    
+리턴값으로 인스턴스를 생성해주기만 하면 되기때문에 클래스 자기자신  
+Time(hour,minute,second)를 적는다.  
 
