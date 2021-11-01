3dcbaa028101166ca9174f2d207bbab52646e989
myunghwanKang <myunghwan0421@gmail.com>
2021-11-02 01:34:20 +0900

    Update test.py

--- _posts/CS/datastructure/test.py
+++ _posts/CS/datastructure/test.py
@@ -25,12 +25,15 @@ class Time:
 
 
 
-time_string = input()
+# time_string = input()
 
-if Time.is_time_valid(time_string): #정적메서드, 입력받아 truefalse리턴
-    t = Time.from_string(time_string) #t라는 인스턴스를 만들어줘야한다. 
-    #인스턴스를 어떻게 만들었었지?-->t = Time(hour,minute,second)
-    #from_string은 리턴값으로 
-    print(t.hour, t.minute, t.second)
-else:
-    print('잘못된 시간 형식입니다.')
+# if Time.is_time_valid(time_string): #정적메서드, 입력받아 truefalse리턴
+#     t = Time.from_string(time_string) #t라는 인스턴스를 만들어줘야한다. 
+#     #인스턴스를 어떻게 만들었었지?-->t = Time(hour,minute,second)
+#     #from_string은 리턴값으로 
+#     print(t.hour, t.minute, t.second)
+# else:
+#     print('잘못된 시간 형식입니다.')
+
+
+print(Time.__name__)
